64bd56f7975630d59801b7ff305d392919f8c0cd
AllenJoeG <allen.joeg@gmail.com>
2021-10-13 12:08:00 -0500

    Update 0.1 initial functions in place

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item){
+  basket.push(item);
+  return true;
+}
+
+function listItems(array){
+  for (let item in array){
+    console.log(item);
+  }
+}
+
+function empty(){
+  basket = [];
+  return console.log('You have emptied your basket.')
+}
